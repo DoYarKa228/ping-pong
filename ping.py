1553822ecfac194d294e484171d79192b1e96c5e
DoYarKa228 <kataevkiruha@gmail.com>
2025-04-28 20:04:38 +0500

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,30 @@
+import pygame
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.speed = player_speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#класс спрайта игрока
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def fire(self):
+        bullet = Bullet('bullet_11125513.png', self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
